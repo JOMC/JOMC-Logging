160260cbaa2041c7d13b4e93bd951226874514ea
Christian Schulte <cs@schulte.it>
2009-07-26 23:26:01 +0000

    o Moved to components/jomc-logging/trunk directory.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/components/jomc-logging/trunk@314 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

---
+++ jomc-logging-log4j-logging/src/main/java/org/jomc/logging/ri/log4j/Log4JObjectManagementListener.java
@@ -0,0 +1,142 @@
+// SECTION-START[License Header]
+/*
+ *   Copyright (c) 2009 The JOMC Project
+ *   Copyright (c) 2005 Christian Schulte <schulte2005@users.sourceforge.net>
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     o Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     o Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE JOMC PROJECT AND CONTRIBUTORS "AS IS"
+ *   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE JOMC PROJECT OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *   OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   $Id$
+ *
+ */
+// SECTION-END
+package org.jomc.logging.ri.log4j;
+
+import java.util.logging.Level;
+
+// SECTION-START[Implementation Comment]
+/**
+ * {@code ObjectManagementLister} backed by Log4J logging.
+ * <p><b>Specifications</b><ul>
+ * <li>{@code org.jomc.spi.Listener} {@code 1.0}<blockquote>
+ * Object applies to Singleton scope.</blockquote></li>
+ * </ul></p>
+ * <p><b>Dependencies</b><ul>
+ * <li>"{@link #getLogger Logger}"<blockquote>
+ * Dependency on {@code org.jomc.logging.Logger} at specification level 1.0 applying to Multiton scope bound to an instance.</blockquote></li>
+ * </ul></p>
+ *
+ * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
+ * @version $Id$
+ */
+// SECTION-END
+// SECTION-START[Annotations]
+@javax.annotation.Generated
+(
+    value = "org.jomc.tools.JavaSources",
+    comments = "See http://jomc.sourceforge.net/jomc-tools"
+)
+// SECTION-END
+public class Log4JObjectManagementListener
+implements
+    org.jomc.spi.Listener
+{
+    // SECTION-START[Log4JObjectManagementListener]
+
+    public void onLog( final Level level, final String message, final Throwable throwable )
+    {
+        if ( level != null )
+        {
+            if ( level.equals( Level.CONFIG ) || level.equals( Level.FINE ) )
+            {
+                this.getLogger().debug( message, throwable );
+            }
+            else if ( level.equals( Level.FINER ) || level.equals( Level.FINEST ) )
+            {
+                this.getLogger().trace( message, throwable );
+            }
+            else if ( level.equals( Level.INFO ) )
+            {
+                this.getLogger().info( message, throwable );
+            }
+            else if ( level.equals( Level.SEVERE ) )
+            {
+                this.getLogger().error( message, throwable );
+            }
+            else if ( level.equals( Level.WARNING ) )
+            {
+                this.getLogger().warn( message, throwable );
+            }
+            else
+            {
+                this.getLogger().trace( message, throwable );
+            }
+        }
+    }
+
+    // SECTION-END
+    // SECTION-START[Constructors]
+
+    /** Default implementation constructor. */
+    @javax.annotation.Generated
+    (
+        value = "org.jomc.tools.JavaSources",
+        comments = "See http://jomc.sourceforge.net/jomc-tools"
+    )
+    public Log4JObjectManagementListener()
+    {
+        // SECTION-START[Default Constructor]
+        super();
+        // SECTION-END
+    }
+    // SECTION-END
+    // SECTION-START[Dependencies]
+
+    /**
+     * Gets the {@code Logger} dependency.
+     * <p>This method returns the "{@code JOMC Logging Log4J Logging}" object of the {@code org.jomc.logging.Logger} specification at specification level 1.0.</p>
+     * <p><b>Properties</b><dl>
+     * <dt>"{@code name}"</dt>
+     * <dd>Property of type {@code java.lang.String} with value "org.jomc.ObjectManager".
+     * </dd>
+     * </dl>
+     * @return The {@code Logger} dependency.
+     * @throws org.jomc.ObjectManagementException if getting the dependency instance fails.
+     */
+    @javax.annotation.Generated
+    (
+        value = "org.jomc.tools.JavaSources",
+        comments = "See http://jomc.sourceforge.net/jomc-tools"
+    )
+    private org.jomc.logging.Logger getLogger() throws org.jomc.ObjectManagementException
+    {
+        return (org.jomc.logging.Logger) org.jomc.ObjectManagerFactory.getObjectManager().getDependency( this, "Logger" );
+    }
+    // SECTION-END
+    // SECTION-START[Properties]
+    // SECTION-END
+    // SECTION-START[Messages]
+    // SECTION-END
+}
