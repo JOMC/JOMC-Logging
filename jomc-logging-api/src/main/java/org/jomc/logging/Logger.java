4ce77b88c088ceb91010ab2c0c50897099968c47
Christian Schulte <cs@schulte.it>
2009-12-12 09:10:38 +0000

    [maven-scm-plugin] Build 2009-12-12T09:09:38+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@1158 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
+++ jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
@@ -57,7 +57,7 @@ package org.jomc.logging;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-11/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-12/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public interface Logger
