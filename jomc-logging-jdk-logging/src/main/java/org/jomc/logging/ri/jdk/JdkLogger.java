030f9acbaea422c71348a561de725df7b973eb77
Christian Schulte <cs@schulte.it>
2011-03-27 21:07:25 +0000

    [maven-scm-plugin] Build 2011-03-27T23:07:06+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@3258 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-jdk-logging/src/main/java/org/jomc/logging/ri/jdk/JdkLogger.java
+++ jomc-logging-jdk-logging/src/main/java/org/jomc/logging/ri/jdk/JdkLogger.java
@@ -45,6 +45,29 @@ import java.util.logging.Level;
  * <p>
  *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
  *     <tr class="TableHeadingColor">
+ *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Implementation</font></th>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.logging.ri.jdk.JdkLogger}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Name:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code JOMC Logging JDK Logging}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Flags:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap> {@code stateless}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code 1.1-SNAPSHOT}</td>
+ *     </tr>
+ *   </table>
+ * </p>
+ * <p>
+ *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
+ *     <tr class="TableHeadingColor">
  *       <th align="left" scope="col" colspan="4" nowrap><font size="+2">Specifications</font></th>
  *     </tr>
  *     <tr class="TableSubHeadingColor">
