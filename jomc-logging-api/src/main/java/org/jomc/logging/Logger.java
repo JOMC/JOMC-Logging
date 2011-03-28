1178857a4f25d0b2f9be03a6e87064be0abf394d
Christian Schulte <cs@schulte.it>
2011-03-28 15:13:50 +0000

    [maven-scm-plugin] Build 2011-03-28T17:13:28+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@3277 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
+++ jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
@@ -40,22 +40,28 @@ package org.jomc.logging;
 // <editor-fold defaultstate="collapsed" desc=" Generated Documentation ">
 /**
  * Logs events for a specific component.
- *
  * <p>
- *   This specification declares a multiplicity of {@code One}.
- *   An application assembler may provide either no or one implementation of this specification.
- * </p>
- *
- * <p>
- *   Use of class {@link org.jomc.ObjectManager ObjectManager} is supported for accessing implementations.
- *   <pre>
- * Logger object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Logger.class );
- * Logger object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Logger.class, "<i>implementation name</i>" );
- *   </pre>
- * </p>
- *
- * <p>
- *   This specification does not apply to any scope. A new object is returned whenever requested.
+ *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
+ *     <tr class="TableHeadingColor">
+ *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Specification</font></th>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.logging.Logger}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Multiplicity:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code One}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Scope:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code Multiton}</td>
+ *     </tr>
+ *     <tr>
+ *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
+ *       <td class="TableRowColor" align="left" nowrap>{@code 1.0}</td>
+ *     </tr>
+ *   </table>
  * </p>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
