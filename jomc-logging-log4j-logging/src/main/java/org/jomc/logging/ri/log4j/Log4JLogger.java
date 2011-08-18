5b5e39d540a57bb00b5ab303ce8a0d4dba107afa
Christian Schulte <cs@schulte.it>
2011-08-18 21:39:45 +0000

    [maven-scm-plugin] Build 2011-08-18T23:37:39+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@3656 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-log4j-logging/src/main/java/org/jomc/logging/ri/log4j/Log4JLogger.java
+++ jomc-logging-log4j-logging/src/main/java/org/jomc/logging/ri/log4j/Log4JLogger.java
@@ -42,93 +42,21 @@ import java.util.logging.Level;
 // <editor-fold defaultstate="collapsed" desc=" Generated Documentation ">
 /**
  * Object management and configuration logging system implementation backed by Log4J.
+ *
  * <p>
- *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
- *     <tr class="TableHeadingColor">
- *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Implementation</font></th>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.logging.ri.log4j.Log4JLogger}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Name:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code JOMC Logging Log4J Logging}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Flags:</b></td>
- *       <td class="TableRowColor" align="left" nowrap> {@code stateless}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code 1.1-SNAPSHOT}</td>
- *     </tr>
- *   </table>
- * </p>
- * <p>
- *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
- *     <tr class="TableHeadingColor">
- *       <th align="left" scope="col" colspan="4" nowrap><font size="+2">Specifications</font></th>
- *     </tr>
- *     <tr class="TableSubHeadingColor">
- *       <td align="left" scope="col" nowrap><b>Identifier</b></td>
- *       <td align="left" scope="col" nowrap><b>Class</b></td>
- *       <td align="left" scope="col" nowrap><b>Scope</b></td>
- *       <td align="left" scope="col" nowrap><b>Version</b></td>
- *     </tr>
- *     <tr class="TableRowColor">
- *       <td align="left" nowrap>{@code org.jomc.logging.Logger}</td>
- *       <td align="left" nowrap>{@code org.jomc.logging.Logger}</td>
- *       <td align="left" nowrap>{@code Multiton}</td>
- *       <td align="left" nowrap>{@code 1.0}</td>
- *     </tr>
- *     <tr class="TableRowColor">
- *       <td align="left" nowrap>{@code org.jomc.spi.Listener}</td>
- *       <td align="left" nowrap>{@code org.jomc.spi.Listener}</td>
- *       <td align="left" nowrap>{@code Multiton}</td>
- *       <td align="left" nowrap>{@code 1.0}</td>
- *     </tr>
- *   </table>
- * </p>
- * <p>
- *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
- *     <tr class="TableHeadingColor">
- *       <th align="left" scope="col" colspan="4" nowrap><font size="+2">Properties</font></th>
- *     </tr>
- *     <tr class="TableSubHeadingColor">
- *       <td align="left" scope="col" nowrap><b>Name</b></td>
- *       <td align="left" scope="col" nowrap><b>Type</b></td>
- *       <td align="left" scope="col" nowrap><b>Flags</b></td>
- *       <td align="left" scope="col" nowrap><b>Documentation</b></td>
- *     </tr>
- *     <tr class="TableRowColor">
- *       <td align="left" valign="top" nowrap>{@link #getName name}</td>
- *       <td align="left" valign="top" nowrap>{@code java.lang.String}</td>
- *       <td align="left" valign="top" nowrap>{@code none}</td>
- *       <td align="left" valign="top">Name of the component events are logged for.</td>
- *     </tr>
- *   </table>
- * </p>
- * <p>
- *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
- *     <tr class="TableHeadingColor">
- *       <th align="left" scope="col" colspan="3" nowrap><font size="+2">Dependencies</font></th>
- *     </tr>
- *     <tr class="TableSubHeadingColor">
- *       <td align="left" scope="col" nowrap><b>Name</b></td>
- *       <td align="left" scope="col" nowrap><b>Flags</b></td>
- *       <td align="left" scope="col" nowrap><b>Description</b></td>
- *     </tr>
- *     <tr class="TableRowColor">
- *       <td align="left" valign="top" nowrap>{@link #getObjectManagementLogger ObjectManagementLogger}</td>
- *       <td align="left" valign="top" nowrap>{@code none}</td>
- *       <td align="left" valign="top">Dependency on the {@code 'JOMC Logging Log4J Logging'} object of the {@code 'org.jomc.logging.Logger'} {@code (org.jomc.logging.Logger)} specification at specification level 1.0 bound to an instance.</td>
- *     </tr>
- *   </table>
+ *   This implementation is identified by identifier {@code <org.jomc.logging.ri.log4j.Log4JLogger>}.
+ *   It provides objects named {@code <JOMC Logging Log4J Logging>} of the following specifications:
+ *
+ *   <ul>
+ *     <li>{@code <org.jomc.logging.Logger>} at specification level 1.0.</li>
+ *     <li>{@code <org.jomc.spi.Listener>} at specification level 1.0.</li>
+ *   </ul>
+ *
+ *   No state is retained across operations due to flag {@code <stateless>}.
  * </p>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
- * @version $Id$
+ * @version 1.1-SNAPSHOT
  */
 // </editor-fold>
 // SECTION-END
@@ -326,9 +254,11 @@ public final class Log4JLogger
     // <editor-fold defaultstate="collapsed" desc=" Generated Dependencies ">
 
     /**
-     * Gets the {@code ObjectManagementLogger} dependency.
-     * <p>This method returns the {@code 'JOMC Logging Log4J Logging'} object of the {@code 'org.jomc.logging.Logger'} {@code (org.jomc.logging.Logger)} specification at specification level 1.0.</p>
-     * <p>That specification does not apply to any scope. A new object is returned whenever requested and bound to this instance.</p>
+     * Gets the {@code <ObjectManagementLogger>} dependency.
+     * <p>
+     *   This method returns the {@code <JOMC Logging Log4J Logging>} object of the {@code <org.jomc.logging.Logger>} specification at specification level 1.0.
+     *   That specification does not apply to any scope. A new object is returned whenever requested and bound to this instance.
+     * </p>
      * <p><strong>Properties:</strong>
      *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
      *     <tr class="TableSubHeadingColor">
@@ -337,7 +267,7 @@ public final class Log4JLogger
      *       <th align="left" scope="col" nowrap><b>Documentation</b></th>
      *     </tr>
      *     <tr class="TableRow">
-     *       <td align="left" valign="top" nowrap>{@code name}</td>
+     *       <td align="left" valign="top" nowrap>{@code <name>}</td>
      *       <td align="left" valign="top" nowrap>{@code java.lang.String}</td>
      *       <td align="left" valign="top">Name of the logger object management events are logged with.</td>
      *     </tr>
@@ -359,7 +289,7 @@ public final class Log4JLogger
     // <editor-fold defaultstate="collapsed" desc=" Generated Properties ">
 
     /**
-     * Gets the value of the {@code name} property.
+     * Gets the value of the {@code <name>} property.
      * @return Name of the component events are logged for.
      * @throws org.jomc.ObjectManagementException if getting the property instance fails.
      */
