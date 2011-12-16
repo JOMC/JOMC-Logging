9e903daa99f789945ffe68d50d712e71c3911ebb
Christian Schulte <cs@schulte.it>
2011-12-16 10:42:38 +0000

    [maven-scm-plugin] Build 2011-12-16T10:42:10+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@4074 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-log4j-logging/src/main/java/org/jomc/logging/ri/log4j/Log4JLogger.java
+++ jomc-logging-log4j-logging/src/main/java/org/jomc/logging/ri/log4j/Log4JLogger.java
@@ -41,18 +41,10 @@ import java.util.logging.Level;
 /**
  * Object management and configuration logging system implementation backed by Log4J.
  *
- * <p>
- *   This implementation is identified by {@code <org.jomc.logging.ri.log4j.Log4JLogger>}.
- * </p>
- * <p>
- *   It provides objects named {@code <JOMC Logging Log4J Logging>} of the following specifications:
- *
- *   <ul>
- *     <li>{@code <org.jomc.logging.Logger>} at specification level 1.0.</li>
- *     <li>{@code <org.jomc.spi.Listener>} at specification level 1.0.</li>
- *   </ul>
- * </p>
  * <dl>
+ *   <dt><b>Identifier:</b></dt><dd>org.jomc.logging.ri.log4j.Log4JLogger</dd>
+ *   <dt><b>Name:</b></dt><dd>JOMC Logging Log4J Logging</dd>
+ *   <dt><b>Implements:</b></dt><dd>org.jomc.logging.Logger@1.0, org.jomc.spi.Listener@1.0</dd></dt>
  *   <dt><b>Abstract:</b></dt><dd>No</dd>
  *   <dt><b>Final:</b></dt><dd>No</dd>
  *   <dt><b>Stateless:</b></dt><dd>Yes</dd>
