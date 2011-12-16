9f15a173d457b4dca499cbf6b01abfb607a7910b
Christian Schulte <cs@schulte.it>
2011-12-16 10:36:09 +0000

    [maven-scm-plugin] Build 2011-12-16T10:35:53+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@4067 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
+++ jomc-logging-api/src/main/java/org/jomc/logging/Logger.java
@@ -39,25 +39,17 @@ package org.jomc.logging;
 /**
  * Logs events for a specific component.
  *
- * <p>
- *   This specification is identified by {@code <org.jomc.logging.Logger>}.
- * </p>
- * <p>
- *   Use of class {@code ObjectManager} is supported for accessing implementations.
- *   <blockquote><pre>
- * Logger object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Logger.class );
- * Logger object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Logger.class, "<i>implementation name</i>" );
- *   </pre></blockquote>
- * </p>
  * <dl>
+ *   <dt><b>Identifier:</b></dt><dd>org.jomc.logging.Logger</dd>
  *   <dt><b>Multiplicity:</b></dt><dd>One</dd>
  *   <dt><b>Scope:</b></dt><dd>None</dd>
  * </dl>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
  * @version 1.0
+ * @see org.jomc.ObjectManager#getObject(java.lang.Class) getObject(Logger.class)
+ * @see org.jomc.ObjectManager#getObject(java.lang.Class,java.lang.String) getObject(Logger.class, "<i>implementation name</i>")
  * @see org.jomc.ObjectManagerFactory
- * @see org.jomc.ObjectManager
  */
 // </editor-fold>
 // SECTION-END
