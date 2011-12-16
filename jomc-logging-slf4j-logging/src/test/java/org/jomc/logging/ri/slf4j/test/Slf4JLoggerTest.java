2dfd6f5c2f5f36dd0631aebbedb31fcc30f05f60
Christian Schulte <cs@schulte.it>
2011-12-16 10:49:48 +0000

    [maven-scm-plugin] Build 2011-12-16T10:49:23+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-logging/trunk@4077 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-logging-slf4j-logging/src/test/java/org/jomc/logging/ri/slf4j/test/Slf4JLoggerTest.java
+++ jomc-logging-slf4j-logging/src/test/java/org/jomc/logging/ri/slf4j/test/Slf4JLoggerTest.java
@@ -41,10 +41,9 @@ import org.jomc.logging.it.LoggerTest;
 /**
  * Test cases for class {@code org.jomc.logging.ri.slf4j.Slf4JLogger}.
  *
- * <p>
- *   This implementation is identified by {@code <org.jomc.logging.ri.slf4j.test.Slf4JLoggerTest>}.
- * </p>
  * <dl>
+ *   <dt><b>Identifier:</b></dt><dd>org.jomc.logging.ri.slf4j.test.Slf4JLoggerTest</dd>
+ *   <dt><b>Name:</b></dt><dd>JOMC Logging Slf4J Logging</dd>
  *   <dt><b>Abstract:</b></dt><dd>No</dd>
  *   <dt><b>Final:</b></dt><dd>No</dd>
  *   <dt><b>Stateless:</b></dt><dd>Yes</dd>
